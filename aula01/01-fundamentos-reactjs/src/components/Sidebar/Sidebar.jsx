0551146350800f740951e8ae76f01773e43986d5
Matheus Cassioli <matheuscassioli@hotmail.com>
2024-10-04 01:22:07 -0300

    hotfix(sidebar) : bug no estado default da imagem vazio

--- aula01/01-fundamentos-reactjs/src/components/Sidebar/Sidebar.jsx
+++ aula01/01-fundamentos-reactjs/src/components/Sidebar/Sidebar.jsx
@@ -5,12 +5,12 @@ import Avatar from '../Avatar/Avatar'
 
 const Sidebar = () => {
 
-    const [dinamicImage, setDinamicImage] = useState('')
+    const [dinamicImage, setDinamicImage] = useState("https://plus.unsplash.com/premium_photo-1681433343461-b2578d44f7ac?q=80&w=1503&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     const dinamicsUrls = [
-        "https://plus.unsplash.com/premium_photo-1681433343461-b2578d44f7ac?q=80&w=1503&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "https://media.istockphoto.com/id/1404221665/pt/foto/abstract-defocused-lens-color-gradient-on-black-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=Sr_bYAMsFKh2yYG97OXY711oiA16dB31mE8phCzmlWU=",
         "https://plus.unsplash.com/premium_photo-1668650610258-043278428a93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cHVycGxlfGVufDB8fDB8fHww",
-        "https://images.unsplash.com/photo-1617957689187-997a52cd5e76?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHB1cnBsZXxlbnwwfHwwfHx8MA%3D%3D"
+        "https://images.unsplash.com/photo-1617957689187-997a52cd5e76?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHB1cnBsZXxlbnwwfHwwfHx8MA%3D%3D",
+        "https://plus.unsplash.com/premium_photo-1681433343461-b2578d44f7ac?q=80&w=1503&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     ]
 
     useEffect(() => {
